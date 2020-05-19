Mask prediction after stop token during loss calculation + fix stop tokens (#19)
* Fix config bug

* Fix stop tokens

* Mask prediction output during loss calculation

* Fix reformer workers

* Remove unnecessary print

reformer_tts/dataset/utils.py
@@ -18,11 +18,28 @@ def custom_sequence_padder(batch):
     start_token = torch.zeros((spectrograms.shape[0], 1, spectrograms.shape[2]), device=torch.device('cpu'))
     spectrograms = torch.cat([start_token, spectrograms], dim=1)
 
-    length_ind = torch.tensor([len(e['spectrogram']) - 1 for e in batch], dtype=torch.long, device=torch.device('cpu'))
+    length_ind = torch.tensor([len(e['spectrogram']) for e in batch], dtype=torch.long, device=torch.device('cpu'))
     lengths_matrix = torch.zeros((spectrograms.shape[0], spectrograms.shape[1]), device=torch.device('cpu'))
     lengths_matrix[torch.arange(len(length_ind), device=torch.device('cpu')), length_ind] = 1
+    stop_tokens = lengths_matrix[:, 1:]
 
-    return {"phonemes": phonemes, "spectrogram": spectrograms, "stop_tokens": lengths_matrix[:, 1:]}
+    mask_rows = []
+    max_spectrogram_length = spectrograms.shape[1] - 1
+    for length in length_ind:
+        ones = torch.ones(length, device=torch.device('cpu'))
+        zeros = torch.zeros(max_spectrogram_length - length, device=torch.device('cpu'))
+        mask_row = torch.cat([ones, zeros])
+        mask_row = mask_row.unsqueeze(1).repeat(1, spectrograms.shape[2])
+        mask_rows.append(mask_row)
+    loss_mask = torch.stack(mask_rows)
+    assert loss_mask.shape == spectrograms[:, 1:, :].shape
+
+    return {
+        "phonemes": phonemes,
+        "spectrogram": spectrograms,
+        "stop_tokens": stop_tokens,
+        "loss_mask": loss_mask,
+    }
 
 
 def get_subset_lengths(length, split_percentages):